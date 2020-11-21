ddbd615d4c64ef95ff01fc94bdcabaf07e14ac69
hskwakr <33633391+hskwakr@users.noreply.github.com>
2020-11-22 01:38:39 +0900

    Implement sample codes
    BeforeOCP and a Test

---
+++ PracticeOpenClosedPrinciple/Sample1/BeforeOCP/Invoice.cs
@@ -0,0 +1,27 @@
+﻿namespace PracticeOpenClosedPrinciple.Sample1.BeforeOCP
+{
+    public class Invoice
+    {
+        public double GetInvoiceDiscount(double amount, InvoiceType invoiceType)
+        {
+            double finalAmount = 0;
+
+            if (invoiceType == InvoiceType.FinalInvoice)
+            {
+                finalAmount = amount - 100;
+            }
+            else if (invoiceType == InvoiceType.ProposedInvoice)
+            {
+                finalAmount = amount - 50;
+            }
+
+            return finalAmount;
+        }
+    }
+
+    public enum InvoiceType
+    {
+        FinalInvoice,
+        ProposedInvoice
+    }
+}
---
+++ PracticeOpenClosedPrinciple/Sample1/README.md
@@ -0,0 +1,2 @@
+## Sample 1
+This sample is practiced using the article of [Open-Closed Principle in C# example - Dot Net Tutorials](https://dotnettutorials.net/lesson/open-closed-principle/ "Open-Closed Principle in C# example - Dot Net Tutorials")
