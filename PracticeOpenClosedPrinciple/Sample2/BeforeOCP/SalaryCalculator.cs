9575937e0728920ba2f9437e8eabac66f1d52f38
hskwakr <33633391+hskwakr@users.noreply.github.com>
2020-12-02 15:58:27 +0900

    Add codes of BeforeOCP in sample2
    and a test

---
+++ PracticeOpenClosedPrinciple/Sample2/BeforeOCP/SalaryCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace PracticeOpenClosedPrinciple.Sample2.BeforeOCP
+{
+    public class SalaryCalculator
+    {
+        private readonly IEnumerable<DeveloperReport> _developerReports;
+
+        public SalaryCalculator(List<DeveloperReport> developerReports)
+        {
+            _developerReports = developerReports;
+        }
+
+        public double CalculateTotalSalaries()
+        {
+            double totalSalaries = 0D;
+
+            foreach (var devReport in _developerReports)
+            {
+                totalSalaries += devReport.HourlyRate * devReport.WorkingHours;
+            }
+
+            return totalSalaries;
+        }
+    }
+}
