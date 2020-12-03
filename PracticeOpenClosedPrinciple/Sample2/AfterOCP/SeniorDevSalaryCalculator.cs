49a26e4c86754829455815f833901b885ea9edae
hskwakr <33633391+hskwakr@users.noreply.github.com>
2020-12-03 09:59:50 +0900

    Add codes of AfterOCP in Sample2
    and a test about CalculateTotalSalaries

---
+++ PracticeOpenClosedPrinciple/Sample2/AfterOCP/SeniorDevSalaryCalculator.cs
@@ -0,0 +1,14 @@
+﻿namespace PracticeOpenClosedPrinciple.Sample2.AfterOCP
+{
+    public class SeniorDevSalaryCalculator : BaseSalaryCalculator
+    {
+        public SeniorDevSalaryCalculator(DeveloperReport report)
+            : base(report)
+        { }
+
+        public override double CalculateSalary()
+        {
+            return Report.HourlyRate * Report.WorkingHours * 1.2;
+        }
+    }
+}
---
+++ Test/Sample2/AfterOCP/TestSalaryCalculator.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+using PracticeOpenClosedPrinciple.Sample2.AfterOCP;
+using System.Collections.Generic;
+
+namespace Test.Sample2.AfterOCP
+{
+    public class TestSalaryCalculator
+    {
+        public class CalculateTotalSalariesMethod
+        {
+            [Fact]
+            public void CalculateTotalSalaries_Calculated()
+            {
+                List<BaseSalaryCalculator> data = new List<BaseSalaryCalculator>
+                {
+                    new JuniorDevSalaryCalculator(new DeveloperReport{ Id = 1, Name = "ho", Level = "Junior", HourlyRate = 1.0, WorkingHours = 100 }),
+                    new SeniorDevSalaryCalculator(new DeveloperReport{ Id = 2, Name = "so", Level = "Senior", HourlyRate = 0.5, WorkingHours = 200 }),
+                    new JuniorDevSalaryCalculator(new DeveloperReport{ Id = 3, Name = "ka", Level = "Junior", HourlyRate = 2.0, WorkingHours = 50 }),
+                    new SeniorDevSalaryCalculator(new DeveloperReport{ Id = 4, Name = "wa", Level = "Senior", HourlyRate = 2.5, WorkingHours = 40 })
+                };
+
+                SalaryCalculator calc = new SalaryCalculator(data);
+
+                double actual = calc.CalculateTotalSalaries();
+                double expected = 440D;
+                Assert.Equal(expected, actual);
+            }
+        }
+    }
+}
