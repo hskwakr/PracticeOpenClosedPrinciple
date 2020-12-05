55995359c988b00c89af195e9a00371d5ee87a4c
hskwakr <33633391+hskwakr@users.noreply.github.com>
2020-12-05 15:13:16 +0900

    Add codes of AfterOCP in Sample3
    and  tests

---
+++ PracticeOpenClosedPrinciple/Sample3/AfterOCP/ComputerMonitor.cs
@@ -0,0 +1,22 @@
+﻿namespace PracticeOpenClosedPrinciple.Sample3.AfterOCP
+{
+    public enum MonitorType
+    {
+        OLED,
+        LCD,
+        LED
+    }
+    
+    public enum Screen
+    {
+        WideScreen,
+        CurvedScreen
+    }
+
+    public class ComputerMonitor
+    {
+        public string Name { get; set; }
+        public MonitorType Type { get; set; }
+        public Screen Screen { get; set; }
+    }
+}
---
+++ PracticeOpenClosedPrinciple/Sample3/AfterOCP/ISpecification.cs
@@ -0,0 +1,7 @@
+﻿namespace PracticeOpenClosedPrinciple.Sample3.AfterOCP
+{
+    public interface ISpecification<T>
+    {
+        bool IsSatisfied(T item);
+    }
+}
